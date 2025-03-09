ad92792c17ffaf2b319c3aa5933e1c7f2e3eb045
spiceglide <spiceglide@gmail.com>
2025-03-09 19:46:55 +0000

    Added player animation

--- player.gd
+++ player.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 signal hit
 
-@export var speed = 500
-@export var multiplier = 1.75
+@export var speed = 1500
+@export var multiplier = 0.3
+var last_dir = "down";
 
 
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
+	$AnimatedSprite2D.play()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,33 +27,24 @@ func _physics_process(delta: float) -> void:
 	# Determine direction
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
+		last_dir = "up"
 		velocity.y -= 1
 	if Input.is_action_pressed("move_down"):
+		last_dir = "down"
 		velocity.y += 1
 	if Input.is_action_pressed("move_left"):
+		last_dir = "left"
 		velocity.x -= 1
 	if Input.is_action_pressed("move_right"):
+		last_dir = "right"
 		velocity.x += 1
 
 	# Normalise velocity
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed * mult
+		$AnimatedSprite2D.animation = last_dir
 		move_and_slide()
-		#$AnimatedSprite2D.play()
 	else:
-		pass
-		#$AnimatedSprite2D.play()
-
-	# Animations
-	if velocity.y < 0:
-		pass
-		#$AnimatedSprite2D.animation = "up"
-	elif velocity.y > 0:
-		pass
-		#$AnimatedSprite2D.animation = "down"
-	elif velocity.x < 0:
-		pass
-		#$AnimatedSprite2D.animation = "left"
-	elif velocity.x > 0:
-		pass
-		#$AnimatedSprite2D.animation = "right"
+		$AnimatedSprite2D.animation = last_dir + "_idle"
+	
+	$AnimatedSprite2D.play()

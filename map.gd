a0a2d2140e2564faabb5261aae713d68e2953f40
spiceglide <spiceglide@gmail.com>
2025-03-09 17:02:31 +0000

    Added second possible scene

--- map.gd
+++ map.gd
@@ -1,15 +1,18 @@
 extends Node2D
 
-@export var grid_size = [3, 3]
+@export var grid_size = [9, 9]
 var scene_size = [3000, 3000]
+var scenes = [preload("res://scene1.tscn"), preload("res://scene2.tscn")]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var scene = preload("res://scene.tscn")
+	var scene
+	var instance
 	
 	for x in range(grid_size[0]):
 		for y in range(grid_size[1]):
-			var instance = scene.instantiate()
+			scene = scenes.pick_random()
+			instance = scene.instantiate()
 			instance.position.x = scene_size[0] * x
 			instance.position.y = scene_size[1] * y
 			add_child(instance)

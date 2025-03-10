ef63b008f5bb0c2a50fafbe18ae16319effb7d70
spiceglide <spiceglide@gmail.com>
2025-03-10 16:33:06 +0000

    Added ability to interact with objects

---
+++ interaction.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact"):
+		for area: Area2D in get_overlapping_areas():
+			print("interaction!")

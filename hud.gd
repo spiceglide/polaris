6b04f79230e24a27ce5586d53d01d771952af7f2
spiceglide <spiceglide@gmail.com>
2025-03-12 19:08:38 +0000

    Added custom font

--- hud.gd
+++ hud.gd
@@ -11,6 +11,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	timer += 1
 	
 	if (timer > announcement_timeout) and ($AnnouncementContainer/Announcement.text == announcement):

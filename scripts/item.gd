1bf84279009edd0e515822ec621c82ccfd5a8bc4
spiceglide <spiceglide@gmail.com>
2025-03-14 10:54:57 +0000

    Refactored inventory to use resizeable containers

--- scripts/item.gd
+++ scripts/item.gd
@@ -1,13 +1,15 @@
-extends Node2D
+extends Container
 class_name Item
 
 @export var id: String = ""
 @export var title: String = ""
 @export var description: String = ""
+@export var anim: String = "type1"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Sprite.animation = anim
+	$Sprite.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- scripts/slot.gd
+++ scripts/slot.gd
@@ -4,7 +4,7 @@ class_name InventorySlot
 @export var item: Item
 
 signal slot_click(which: InventorySlot)
-signal slot_hovered(which: InventorySlot)
+signal slot_hovered(which: InventorySlot, is_hover: bool)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,11 +15,11 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_button_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.pressed
+	if event is InputEventMouseButton and event.pressed:
+		slot_click.emit(self)
 
 func _on_button_mouse_entered() -> void:
-	pass # Replace with function body.
-
+	slot_hovered.emit(self, true)
 
 func _on_button_mouse_exited() -> void:
-	pass # Replace with function body.
+	slot_hovered.emit(self, false)

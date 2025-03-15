77a33676f59052c3417bd1d374e68beae07b77fa
spiceglide <spiceglide@gmail.com>
2025-03-15 11:23:43 +0000

    Moved inventory to JSON

--- scripts/item.gd
+++ scripts/item.gd
@@ -5,9 +5,19 @@ class_name Item
 @export var title: String = ""
 @export var description: String = ""
 
-var item_map = {"rock": {"name": "Rock", "description": '"Better than nothing"'}}
+var item_map
 
 func _ready() -> void:
+	var file = FileAccess.open("res://data/items.json", FileAccess.READ)
+	var json = file.get_as_text()
+	
+	var reader = JSON.new()
+	var error = reader.parse(json)
+	if error == OK:
+		item_map = reader.data
+	else:
+		print("JSON Parse Error: %s at %s" % [reader.get_error_message(), reader.get_error_line()])
+	
 	add_to_group("items")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

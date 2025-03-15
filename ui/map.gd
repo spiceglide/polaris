5b3b34374b4c7ec8dc78a88f4963875a26a975e3
spiceglide <spiceglide@gmail.com>
2025-03-15 17:20:03 +0000

    Deloading distant scenes

--- ui/map.gd
+++ ui/map.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
-@export var grid_size = [3, 3]
-var scene_size = [24*640, 24*640]
+const DIMS = 24*640
+const RENDER_DISTANCE = 2
+
+@export var grid_size = [9, 9]
+var scene_size = [DIMS, DIMS]
 #var scenes = [preload("res://scenes/scene1.tscn"), preload("res://scenes/scene2.tscn"), preload("res://scenes/scene3.tscn")]
-var scenes = [preload("res://screens/Level_Snow_1.tscn")]
+var available_scenes = [preload("res://screens/Level_Snow_1.tscn")]
+var scenes = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,13 +15,41 @@ func _ready() -> void:
 	var instance
 	
 	for x in range(grid_size[0]):
+		var temp_scenes = []
 		for y in range(grid_size[1]):
-			scene = scenes.pick_random()
+			scene = available_scenes.pick_random()
 			instance = scene.instantiate()
+			
 			instance.position.x = scene_size[0] * x
 			instance.position.y = scene_size[1] * y
+			instance.visible = false
+			
+			temp_scenes.append(instance)
 			add_child(instance)
+		scenes.append(temp_scenes)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	_cull_scenes()
+
+func _cull_scenes():
+	var player_pos = Player.position
+	var bounds = [
+		Player.position.x - (RENDER_DISTANCE * DIMS),
+		Player.position.x + (RENDER_DISTANCE * DIMS),
+		Player.position.y - (RENDER_DISTANCE * DIMS),
+		Player.position.y + (RENDER_DISTANCE * DIMS),
+	]
+	
+	var printthing = ""
+	for x in range(grid_size[0]):
+		for y in range(grid_size[1]):
+			if (x*DIMS < bounds[0]) or (x*DIMS > bounds[1] or (y*DIMS < bounds[2]) or (y*DIMS > bounds[3])):
+				scenes[x][y].visible = false
+			else:
+				scenes[x][y].visible = true
+			
+			printthing += ('O' if scenes[x][y].visible else 'X')
+		printthing += "\n"
+	print(printthing)
+	print()

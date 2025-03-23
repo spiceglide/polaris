eb05e62a8d88c1865ae7ba52b219c475b288f746
spiceglide <spiceglide@gmail.com>
2025-03-23 18:03:54 +0000

    Frog now jumps away from player

--- characters/creatures/frog_ai.gd
+++ characters/creatures/frog_ai.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
-@export var speed = 6000
+@export var speed = 4000
 @export var leap_duration = 12
+@export var leap_count = 2
 var state: State = State.Idle
 var leap_time = 0
 var leap_direction = Vector2.ZERO
+var last_player_pos = Vector2.ZERO
 
 enum State {
 	Idle,
@@ -17,51 +19,44 @@ func _ready() -> void:
 	$AnimatedSprite2D.play()
 	
 func _process(delta: float) -> void:
-	if leap_time > leap_duration:
+	if leap_time > (leap_duration * leap_count):
 		finish_leap()
 		
 	if state == State.Danger:
-		print(leap_time)
 		if leap_time == 0:
 			start_leap()
-		else:
-			leap_time += 1
 	
-	print(leap_time)
+	if leap_time > 0:
+		leap_time += 1
+	
+	if leap_time == 0:
+		match state:
+			State.Idle:
+					$AnimatedSprite2D.animation = "ribbit"
+			State.Alert:
+					$AnimatedSprite2D.animation = "idle"
 
 func _physics_process(delta: float) -> void:
 	# Determine direction
-	velocity = Vector2.ZERO
-	velocity.x += leap_direction.x
-	velocity.y += leap_direction.y
+	velocity = leap_direction
 
 	# Normalise velocity
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		move_and_slide()
 
-func set_state(state: State):
-	self.state = state
-	
-	match state:
-		State.Idle:
-				$AnimatedSprite2D.animation = "ribbit"
-		State.Alert:
-				$AnimatedSprite2D.animation = "idle"
-
 func start_leap():
-	var left_right = randf()
-	var up_down = randf()
+	var threat_direction = self.position.direction_to(last_player_pos)
+	leap_time = 1
 	
-	leap_time = 0
-	
-	if left_right < 0.5:
+	if threat_direction.x < 0:
 		leap_direction.x = 1
-		$AnimatedSprite2D.flip_h = true
-	else:
 		$AnimatedSprite2D.flip_h = false
+	else:
 		leap_direction.x = -1
-	
+		$AnimatedSprite2D.flip_h = true
+
+	var up_down = randf()
 	if up_down < 0.33:
 		leap_direction.y = 1
 	elif up_down < 0.66:
@@ -75,19 +70,24 @@ func start_leap():
 func finish_leap():
 	leap_direction = Vector2.ZERO
 	leap_time = 0
+	$AnimatedSprite2D.flip_h = not $AnimatedSprite2D.flip_h
 
 func _on_alert_range_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		set_state(State.Alert)
+		self.state = State.Alert
+		last_player_pos = body.position
 
 func _on_alert_range_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
-		set_state(State.Idle)
+		self.state = State.Idle
+		last_player_pos = body.position
 
 func _on_danger_range_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		set_state(State.Danger)
+		self.state = State.Danger
+		last_player_pos = body.position
 
 func _on_danger_range_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
-		set_state(State.Alert)
+		self.state = State.Alert
+		last_player_pos = body.position

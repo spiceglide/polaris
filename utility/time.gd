917c086b6cb82e1c46e5801cc17de5290926c297
spiceglide <spiceglide@gmail.com>
2025-03-29 16:05:57 +0000

    More tweaks?

--- utility/time.gd
+++ utility/time.gd
@@ -1,15 +1,13 @@
 extends Node2D
 
-@export var day_length: int = 10
+@export var day_length: int = 20
 var game_time = 0;
 
 func _ready():
 	pass
 
 func _process(delta: float) -> void:
-	game_time += delta
+	if game_time < day_length:
+		game_time += delta
 	
-	if game_time > day_length:
-		game_time = 0
-		
 	RenderingServer.global_shader_parameter_set("game_time", game_time/day_length * 360)

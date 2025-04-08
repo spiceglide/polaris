e0045a6b06d6210ed06ea50cce40aed1925de94d
spiceglide <spiceglide@gmail.com>
2025-04-08 22:14:46 +0100

    Added hunger

--- data/player_data.gd
+++ data/player_data.gd
@@ -17,11 +17,27 @@ var warmth = 100
 var sanity = 100
 
 func _process(delta: float) -> void:
+	# Dead men don't hunger
+	if state == State.Dead:
+		return
+	
+	# Death
 	if health <= 0:
 		state = State.Dead
 	
+	# The night is cold
 	if is_night:
 		warmth -= delta * 5
 	
-	if warmth <= 0:
+	# Hunger
+	hunger -= delta * 0.5
+	
+	# Hungry and cold deaths
+	if (hunger <= 0) or (warmth <= 0):
 		health = 0
+	
+	# Clamp values at 0
+	health = max(health, 0)
+	hunger = max(hunger, 0)
+	warmth = max(warmth, 0)
+	sanity = max(sanity, 0)

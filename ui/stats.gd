a3135940f077ea92ce23e1183dd39816869878f0
spiceglide <spiceglide@gmail.com>
2025-05-27 17:18:23 +0100

    Added textures to progress bars

--- ui/stats.gd
+++ ui/stats.gd
@@ -1,7 +1,12 @@
 extends VBoxContainer
 
+@export var side: String
+
 func _process(delta: float) -> void:
-	$Health.value = PlayerData.health
-	$Hunger.value = PlayerData.hunger
-	$Warmth.value = PlayerData.warmth
-	$Sanity.value = PlayerData.sanity
+	if side == "left":
+		$Health.value = PlayerData.health
+		#$Thirst.value = PlayerData.thirst
+		$Sanity.value = PlayerData.sanity
+	elif side == "right":
+		$Hunger.value = PlayerData.hunger
+		$Warmth.value = PlayerData.warmth

a41988318933de99abb44a8c68244156826cf97e
spiceglide <spiceglide@gmail.com>
2025-08-04 22:58:13 +0100

    Added jittering to lights

--- utility/light.gd
+++ utility/light.gd
@@ -1,8 +1,20 @@
 extends PointLight2D
 
 @export var light: float = 0.8
+@export var jitter_strength: int = 2
+var jitter_direction: float = 1.0
 
 func _process(delta: float) -> void:
+	set_brightness()
+	if self.visible:
+		process_jitter()
+
+func process_jitter() -> void:
+	self.rotation_degrees += jitter_direction
+	if abs(self.rotation_degrees) > jitter_strength:
+		jitter_direction *= -1
+
+func set_brightness() -> void:
 	var night_length = 0.1
 	var gtime = reflect_around(WorldData.game_time/WorldData.day_length, 0.5)
 	gtime = max(min(gtime / night_length, 1.0), 0.0)

89e8a816a833bf42c546b8ab7ac70fc8faae162d
spiceglide <spiceglide@gmail.com>
2025-08-16 17:58:00 +0100

    Implemented idle and walk animations

--- characters/player.gd
+++ characters/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
-var last_dir = "south";
+var direction: Vector2 = Vector2.ZERO
+var interactee = null
+var held = null
+
 var is_holding = false
 var interactable = []
 
@@ -8,7 +11,7 @@ func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
-	$Sprite.set_direction(last_dir, "idle", is_holding)
+	#$Sprite.set_direction(last_dir, "idle", is_holding)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,6 +26,7 @@ func _process(delta: float) -> void:
 	is_holding = false
 	$Light.visible = false
 	
+	"""
 	match PlayerData.state:
 		PlayerData.State.Dead:
 			$Sprite.set_direction(last_dir, "death", is_holding)
@@ -44,6 +48,7 @@ func _process(delta: float) -> void:
 			$Shadow.visible = false
 		PlayerData.State.Cranking:
 			$Sprite.set_direction(last_dir, "crank", is_holding)
+	"""
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -57,29 +62,17 @@ func _move() -> void:
 		mult = PlayerData.multiplier
 
 	# Determine direction
-	velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_left"):
-		last_dir = "west"
-		velocity.x -= 1
-	if Input.is_action_pressed("move_right"):
-		last_dir = "east"
-		velocity.x += 1
-	if Input.is_action_pressed("move_up"):
-		last_dir = "north"
-		velocity.y -= 1
-	if Input.is_action_pressed("move_down"):
-		last_dir = "south"
-		velocity.y += 1
-
+	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
 	# Normalise velocity
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * PlayerData.speed * mult
-		$Sprite.set_direction(last_dir, "walk", is_holding)
+	if direction.length() > 0:
+		self.velocity = direction.normalized() * PlayerData.speed * mult
+		$AnimationTree["parameters/Idle/blend_position"] = direction
+		$AnimationTree["parameters/Walk/blend_position"] = direction
 		if not $WalkingAudio.playing:
 			$WalkingAudio.play()
 		move_and_slide()
 	else:
-		$Sprite.set_direction(last_dir, "idle", is_holding)
 		if $WalkingAudio.playing:
 			$WalkingAudio.stop()
 
@@ -89,7 +82,6 @@ func _input(event):
 			interactable[0].interact(self)
 
 func _on_interaction_body_entered(body: Node2D) -> void:
-	print(body)
 	var interactive = body.get_node("Interaction")
 	if interactive and interactive.is_in_group("interactive"):
 		interactable.append(interactive)
@@ -110,16 +102,16 @@ func _on_animation_finished() -> void:
 		PlayerData.State.PullOut:
 			PlayerData.state = PlayerData.State.Holding
 			is_holding = true
-			last_dir = "south"
 		PlayerData.State.Sleeping:
-			$Sprite.pause()
+			#$Sprite.pause()
 			if $SleepTimer.is_stopped():
 				$SleepTimer.start(5)
 		PlayerData.State.Dead:
-			$Sprite.pause()
+			pass
+			#$Sprite.pause()
 		PlayerData.State.Kill1:
 			PlayerData.state = PlayerData.State.Kill2
-			$Sprite.set_direction(last_dir, "kill_2", is_holding)
+			#$Sprite.set_direction(last_dir, "kill_2", is_holding)
 			$Shadow.visible = false
 		PlayerData.State.Kill3:
 			var item = InventoryData.get_selected_item()
@@ -134,5 +126,5 @@ func _on_animation_looped() -> void:
 	match PlayerData.state:
 		PlayerData.State.Kill3:
 			if $Sprite/Body.animation != "kill_3":
-				$Sprite.set_direction(last_dir, "kill_3", is_holding)
+				#$Sprite.set_direction(last_dir, "kill_3", is_holding)
 				$Shadow.visible = false

5e91152cf972918d67e0eae671e0bfe12c4fbcfb
spiceglide <spiceglide@gmail.com>
2025-08-19 16:18:47 +0100

    Can pull out specific weapon during kill state

--- characters/player/kill_state.gd
+++ characters/player/kill_state.gd
@@ -3,13 +3,18 @@ extends State
 var anim: AnimationPlayer
 var item_sprite: AnimatedSprite2D
 var animal_sprite: AnimatedSprite2D
-var triggered: bool = false
+var stage: int = 0
 
 func enter():
-	triggered = false
+	stage = 0
 	anim = parent_body.get_node("AnimationPlayer")
 	item_sprite = parent_body.get_node("Sprite/Item")
 	animal_sprite = parent_body.get_node("Sprite/Animal")
+	
+	var animal = InventoryData.get_selected_item()
+	if animal in ["frog", "hare"]:
+		animal_sprite.animation = animal
+	
 	anim.play("action/kill_1")
 	anim.queue("action/kill_2")
 
@@ -17,9 +22,18 @@ func exit():
 	return
 
 func update(delta: float):
-	if Input.is_action_just_pressed("use_item") and not triggered:
+	print(animal_sprite.animation)
+	var weapon = InventoryData.get_selected_item()
+	if weapon in ["hatchet", "rock"]:
+		stage = 1
+		item_sprite.animation = weapon + "_kill"
+	else:
+		stage = 0
+		item_sprite.animation = ""
+	
+	if Input.is_action_just_pressed("use_item") and stage == 1:
+		stage = 2
 		anim.play("action/kill_3")
-		triggered = true
 
 func physics_update(delta: float):
 	pass

95cff3486d7ea8d02ed75cb6fac7e073c91e3d76
spiceglide <spiceglide@gmail.com>
2025-08-21 23:26:02 +0100

    Began working towards new item system

--- data/inventory_data.gd
+++ data/inventory_data.gd
@@ -4,11 +4,9 @@ extends Node
 
 var slots: Array[String]
 var selected_slot: int = 0
-var item_map: Dictionary = {}
 var recipe_map: Dictionary = {}
 
 var collectables: Dictionary = {}
-
 var holdable = ["torch", "hatchet"]
 
 # Called when the node enters the scene tree for the first time.
@@ -16,30 +14,12 @@ func _ready() -> void:
 	slots.resize(size)
 	select_slot(0)
 	
-	item_map = _read_items()
-	
 	add_to_group("crafting")
 	
 	if WorldData.get_game_mode() == "overworld":
 		set_item(0, "hatchet")
 		set_item(1, "frog")
 		set_item(4, "sleepingbag")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func _read_items():
-	var file = FileAccess.open("res://data/items.json", FileAccess.READ)
-	var json = file.get_as_text()
-	
-	var reader = JSON.new()
-	var error = reader.parse(json)
-	if error == OK:
-		return reader.data
-	else:
-		print("JSON Parse Error: %s at %s" % [reader.get_error_message(), reader.get_error_line()])
-		return {}
 
 func set_item(slot: int, item: String) -> bool:
 	if not slots[slot]:

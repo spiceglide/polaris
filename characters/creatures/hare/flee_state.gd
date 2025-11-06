fefbc79b0a77c1fe190445de0e0f69b9104fadd0
spiceglide <spiceglide@gmail.com>
2025-11-06 18:48:55 +0100

    Updated hare behaviour

--- characters/creatures/hare/flee_state.gd
+++ characters/creatures/hare/flee_state.gd
@@ -1,65 +1,43 @@
-extends CharacterBody2D
-
-enum EnemyState {
-	Idle,
-	Alert,
-	Flee,
-}
+extends State
 
 @export var speed = 1000
 @export var leap_duration = 12
 @export var leap_count = 5
 
-var state: EnemyState = EnemyState.Idle
-var leap_time = 0
+var notifications: Control
+var anim: AnimationPlayer
+
 var speech_time = 0
 var leap_direction = Vector2.ZERO
-var last_player_pos = Vector2.ZERO
+var endangered: bool = true
 
-func _ready() -> void:
-	$AnimatedSprite2D.animation = "idle"
-	
-func _process(delta: float) -> void:
-	if leap_time > (leap_duration * leap_count):
-		finish_leap()
-		
-	if state == EnemyState.Flee:
-		$AnimatedSprite2D.play()
-		if leap_time == 0:
-			start_leap()
-	
-	if leap_time > 0:
-		leap_time += 1
-	
-	if leap_time == 0:
-		match state:
-			EnemyState.Idle:
-				$AnimatedSprite2D.animation = "idle"
-				$AnimatedSprite2D.play()
-				$Notifications.autochatter = []
-			EnemyState.Alert:
-				$AnimatedSprite2D.animation = "alert"
-				$Notifications.autochatter = []
+func enter() -> void:
+	anim = parent.get_node("AnimationPlayer")
+	notifications = parent.get_node("Notifications")
+	endangered = true
+	start_leap()
 
-func _physics_process(delta: float) -> void:
+func exit() -> void:
+	parent.velocity = Vector2.ZERO
+
+func _physics_process(_delta: float) -> void:
 	# Determine direction
-	velocity = leap_direction
+	parent.velocity = leap_direction
 
 	# Normalise velocity
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		move_and_slide()
+	if parent.velocity.length() > 0:
+		parent.velocity = parent.velocity.normalized() * speed
+		parent.move_and_slide()
 
 func start_leap():
-	var threat_direction = self.position.direction_to(last_player_pos)
-	leap_time = 1
+	var threat_direction = parent.global_position.direction_to(parent.data["last_player_pos"])
 	
 	if threat_direction.x < 0:
 		leap_direction.x = 1
-		$AnimatedSprite2D.flip_h = true
+		anim.play("hop_right")
 	else:
 		leap_direction.x = -1
-		$AnimatedSprite2D.flip_h = false
+		anim.play("hop_left")
 
 	var up_down = randf()
 	if up_down < 0.33:
@@ -68,30 +46,18 @@ func start_leap():
 		leap_direction.y = 0
 	else:
 		leap_direction.y = -1
-		
-	$AnimatedSprite2D.animation = "hop"
 
-func finish_leap():
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name not in ["hop_left", "hop_right"]:
+		return
+	
 	leap_direction = Vector2.ZERO
-	leap_time = 0
-
-func _on_alert_range_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		self.state = EnemyState.Alert
-		last_player_pos = body.position
+	if endangered:
+		start_leap()
+	else:
+		state_transitioned.emit(self, "idle")
 
 func _on_alert_range_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
-		self.state = EnemyState.Idle
-		last_player_pos = body.position
-
-func _on_danger_range_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		self.state = EnemyState.Flee
-		last_player_pos = body.position
-		$Notifications.announce(tr("ENEMY_HARE_FLEE_1"))
-
-func _on_danger_range_body_exited(body: Node2D) -> void:
-	if body.name == "Player":
-		self.state = EnemyState.Alert
-		last_player_pos = body.position
+		parent.data["last_player_pos"] = body.global_position
+		endangered = false
--- characters/creatures/hare/idle_state.gd
+++ characters/creatures/hare/idle_state.gd
@@ -1,9 +1,5 @@
 extends State
 
-@export var speed = 1000
-@export var leap_duration = 12
-@export var leap_count = 5
-
 var anim: AnimationPlayer
 var counter: int = 0;
 
@@ -17,13 +13,15 @@ func _process(delta: float) -> void:
 
 func _on_alert_range_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		parent.data["last_player_pos"] = body.position
+		parent.data["last_player_pos"] = body.global_position
 		state_transitioned.emit(self, "alert")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name.substr(0, 4) == "idle":
-		if anim:
-			if counter == 0:
-				anim.play("idle_earflop")
-			else:
-				anim.play("idle")
+	if anim_name not in ["idle", "idle_earflop"]:
+		return
+	
+	if anim:
+		if counter == 0:
+			anim.play("idle_earflop")
+		else:
+			anim.play("idle")
